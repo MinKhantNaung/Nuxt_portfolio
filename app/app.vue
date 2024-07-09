d3e306cf8e729f80521d1604f6d7783ded780bde
Minkhantnaung@securelink <minkhantnaungkzw@gmail.com>
2024-07-09 17:19:39 +0630

    Add: pages (Nuxt page auto routing)

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,16 @@
 <template>
   <div>
+    <nav>
+      <ul>
+        <li>
+          <NuxtLink to="/">Home</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/about">About</NuxtLink>
+        </li>
+      </ul>
+    </nav>
     <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <NuxtPage />
   </div>
 </template>
