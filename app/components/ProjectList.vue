42e0fedce8018600d6f95642e914adb1effb6d02
Minkhantnaung@securelink <minkhantnaungkzw@gmail.com>
2024-07-11 17:52:03 +0630

    Data Fetching (useFetch for get data)

---
+++ app/components/ProjectList.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <div>Projects</div>
+
+        <section v-if="pending">Loading...</section>
+        <section v-else-if="error">Something went wrong... Try again!</section>
+        <section v-else>Here we display the repos!</section>
+    </div>
+</template>
+
+<script setup>
+const { error, pending, data } = await useFetch('https://api.github.com/users/MinKhantNaung/repos')
+</script>
