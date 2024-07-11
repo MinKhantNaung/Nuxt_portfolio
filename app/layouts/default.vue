967518a318a32c8dfcf340cec1ecef01f3461f4e
Minkhantnaung@securelink <minkhantnaungkzw@gmail.com>
2024-07-11 14:13:47 +0630

    Styling default layout

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,16 +1,24 @@
 <template>
-    <div>
-        <nav>
-            <ul>
-                <li>
-                    <NuxtLink to="/">Home</NuxtLink>
-                </li>
-                <li>
-                    <NuxtLink to="/about">About</NuxtLink>
-                </li>
-            </ul>
-        </nav>
+    <div class="container mx-auto max-w-2xl">
+        <header class="flex justify-between items-center mt-5">
+            <div>
+                <NuxtLink to="/" class="text-xl font-semibold p-2 hover:bg-gray-200">Min Khant Naung</NuxtLink>
+            </div>
 
-        <slot />
+            <nav class="font-mono">
+                <ul class="flex space-x-4">
+                    <li>
+                        <NuxtLink to="/">Main</NuxtLink>
+                    </li>
+                    <li>
+                        <NuxtLink to="/about">About</NuxtLink>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+
+        <main class="p-2">
+            <slot />
+        </main>
     </div>
 </template>
