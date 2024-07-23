4183b51ea7ea56d121015b08e95d7af6a45050ab
Minkhantnaung@securelink <minkhantnaungkzw@gmail.com>
2024-07-23 16:23:29 +0630

    Update: remove scripts in about.vue

--- pages/about.vue
+++ pages/about.vue
@@ -3,34 +3,3 @@
         <ContentDoc path="/about" />
     </article>
 </template>
-
-<script setup>
-function enableCustomLayout() {
-    setPageLayout('another')
-}
-
-useSeoMeta({
-    title: 'About',
-    description: 'This is the About page',
-    ogTitle: 'About',
-    ogDescription: 'This is the About page',
-    ogImage: 'https://example.com/image.png',
-    ogUrl: '[og:url]',
-    twitterTitle: 'About',
-    twitterDescription: '[twitter:description]',
-    twitterImage: '[twitter:image]',
-    twitterCard: 'summary'
-})
-useHead({
-    htmlAttrs: {
-        lang: 'en'
-    },
-    link: [
-        {
-            rel: 'icon',
-            type: 'image/png',
-            href: '/favicon.png'
-        }
-    ]
-})
-</script>
