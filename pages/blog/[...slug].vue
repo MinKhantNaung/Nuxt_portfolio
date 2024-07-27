9164134bd0f850210cc9cec2370f6348fc5fb045
Minkhantnaung@securelink <minkhantnaungkzw@gmail.com>
2024-07-27 17:14:02 +0630

    Table of contents: using slots (Nuxt Content)

--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -1,7 +1,11 @@
 <template>
+    <!-- If use prose, need to add class - max-w-none -->
     <article
-        class="prose dark:prose-invert prose-pre:bg-white dark:prose-pre:bg-gray-800 prose-pre:text-gray-800 dark:prose-pre:text-gray-300">
-        <ContentDoc />
+        class="prose dark:prose-invert max-w-none prose-pre:bg-white dark:prose-pre:bg-gray-800 prose-pre:text-gray-800 dark:prose-pre:text-gray-300">
+        <ContentDoc v-slot="{ doc }">
+            {{ doc.body.toc.links }}
+            <ContentRenderer :value="doc" />
+        </ContentDoc>
     </article>
 </template>
 
