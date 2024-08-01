d0970df9620259bc50d60c2d9a8078a3c5c74c81
Minkhantnaung@securelink <minkhantnaungkzw@gmail.com>
2024-08-01 13:18:28 +0630

    Markdown props, scope slots

--- pages/index.vue
+++ pages/index.vue
@@ -1,42 +1,5 @@
 <template>
-    <div>
-        This is home page.
-    </div>
+    <article class="prose dark:prose-invert max-w-none">
+        <ContentDoc path="/" />
+    </article>
 </template>
-
-<script setup>
-useSeoMeta({
-    title: 'Main Page',
-    description: 'This is the main page',
-    ogTitle: 'Main Page',
-    ogDescription: 'This is the main page',
-    ogImage: 'https://example.com/image.png',
-    ogUrl: '[og:url]',
-    twitterTitle: 'Main Page',
-    twitterDescription: '[twitter:description]',
-    twitterImage: '[twitter:image]',
-    twitterCard: 'summary'
-})
-useHead({
-    htmlAttrs: {
-        lang: 'en'
-    },
-    link: [
-        {
-            rel: 'preconnect',
-            href: 'https://fonts.googleapis.com'
-        },
-        {
-            rel: 'stylesheet',
-            href: 'https://fonts.googleapis.com/css2?family=Roboto&display=swap',
-            crossorigin: ''
-        }
-    ]
-})
-</script>
-
-<style>
-body {
-    font-family: 'Roboto';
-}
-</style>
